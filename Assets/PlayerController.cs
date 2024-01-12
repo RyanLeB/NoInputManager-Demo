53d4168ca8896ff608c165eff191fca8ddb86cf3
Ryan LeBlanc <ryanleb2003@gmail.com>
2024-01-12 09:47:58 -0400

    WASD IJKL

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    // cube speed 
+    float speed = 5.0f; // 5 u/s
+    float stepSpeed = 1.0f; // 1 u steps
+   
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +18,57 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        // GetAxis
+        //float x = Input.GetAxis("Horizontal"); // -1.0..1.0
+        //float y = Input.GetAxis("Vertical"); // -1.0..1.0
+
+        //gameObject.transform.Translate(Vector3.right * Time.deltaTime * speed);
+        //gameObject.transform.Translate(Vector3.forward * Time.deltaTime * speed);
+
+        // translate = move/warp
+
+
+        // WASD
+        if (Input.GetKeyDown(KeyCode.W)) 
+        { 
+            gameObject.transform.Translate(Vector3.forward * stepSpeed);
+        }
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+          gameObject.transform.Translate(Vector3.left * stepSpeed);
+        }
+
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            gameObject.transform.Translate(Vector3.back * stepSpeed);
+        }
+
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            gameObject.transform.Translate(Vector3.right * stepSpeed);
+        }
+
+        // IJKL
+        if (Input.GetKey(KeyCode.I)) 
+        { 
+            gameObject.transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        }
+
+        if (Input.GetKey(KeyCode.J))
+        {
+            gameObject.transform.Translate(Vector3.left * Time.deltaTime * speed);
+        }
+
+        if (Input.GetKey(KeyCode.K))
+        {
+            gameObject.transform.Translate(Vector3.back * Time.deltaTime * speed);
+        }
+
+        if (Input.GetKey(KeyCode.L))
+        {
+            gameObject.transform.Translate(Vector3.right * Time.deltaTime * speed);
+        }
+
     }
 }
